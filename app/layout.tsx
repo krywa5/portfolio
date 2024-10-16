8367edeb42a3ab0f2d29a5bda3a2fed9e0e38951
krywa5 <blocker92@o2.pl>
2024-10-16 17:39:02 +0200

    Contact page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
-import "swiper/css"; // TODO_KW: czy to nie powinno być zaciągane gdzieś wyżej?
+import "swiper/css";
 
 import Header from "@/components/Header";
 import PageTransition from "@/components/PageTransition";
